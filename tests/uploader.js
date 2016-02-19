43d1f90f8933ac308844379f8f133f16561b2662
ykan_ys <ykan_ys@163.com>
2016-02-19 17:19:44 +0800

    add test

--- tests/uploader.js
+++ tests/uploader.js
@@ -5,14 +5,34 @@ let u;
 let info = {
   emit: sinon.spy()
 };
-before(() => {
+beforeEach(() => {
   u = new Uploader(info, 'upload');
 });
 describe('Uploader > doNextAction', () => {
   it('info.shouldStop = true', () => {
     info.shouldStop = true;
-
     let p = u.doNextAction();
     info.emit.should.be.calledWith('stop');
+  });
+  it('如果未上传完，执行uploadChunk', () => {
+    info.shouldStop = false;
+    info.blob = {
+      size: 128 * 64
+    };
+    let c = 122;
+    u._chunkInfos = [c];
+    u.uploadChunk = sinon.spy();
+    let p = u.doNextAction();
+    u.uploadChunk.should.be.calledWith(c);
+  });
+  it('如果上传完，执行makeImgFile', () => {
+    info.shouldStop = false;
+    info.blob = {
+      size: 128 * 64
+    };
+    u._chunkInfos = [];
+    u.makeImgFile = sinon.spy();
+    let p = u.doNextAction();
+    u.makeImgFile.should.be.calledOnce();
   });
 });
